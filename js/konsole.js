b9be6ec31d782bb4ef949af6662966a31a53c4e7
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2014-08-27 20:46:06 -0300

    konsole

---
+++ js/konsole.js
@@ -0,0 +1,19 @@
+// Konsole
+;(function(){
+
+	var container = document.createElement('div');
+
+	container.setAttribute('class','konsole');
+
+	var styl = document.createElement('div');
+
+	container.setAttribute('class','konsole');
+
+
+
+
+
+
+
+
+})();
