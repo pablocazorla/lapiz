69da77114d6f67a4deb4c58a0fc2e66ece4560c1
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2014-08-29 17:43:18 -0300

    animatedShapes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,50 @@
-
-
-
-var cosa = lapiz.sprite('Circle',{
-	x : 15
+var cosa1 = lapiz.sprite('Rectangle',{
+	fillStyle : 'red'
 });
-
-
-cosa.shp();
-
+cosa1.appendTo('my-canvas');
+var cosa2 = lapiz.sprite('Rectangle',{
+	fillStyle : 'blue',
+	strokeStyle : '#FF0',
+	width : 60,
+	height : 40
+});
+var cosa3 = lapiz.sprite('Rectangle',{
+	fillStyle : '#FF0',
+	x : -70,
+	y : -15,
+	width : 140,
+	height : 25
+});
+cosa2.appendTo(cosa1);
+cosa3.appendTo(cosa2);
+cosa1.x = -5;
+cosa1.y = 200;
+cosa1.rotation = 45;
+cosa2.x = -40;
+cosa2.y = -20;
+setInterval(function(){
+	cosa1.rotation += -.5;
+	cosa2.rotation += -1;
+	cosa3.rotation += 4;
+	cosa1.x += 1;
+},30);
+/*
+{
+	'fillStyle':'#FFFFFF',
+	'font':'10px sans-serif',
+	'globalAlpha':1,
+	'globalCompositeOperation':'source-over',
+	'lineCap':'butt',
+	'lineDashOffset':0,
+	'lineJoin':'miter',
+	'lineWidth':1,
+	'miterLimit':10,
+	'shadowBlur':0,
+	'shadowColor':'rgba(0, 0, 0, 0)',
+	'shadowOffsetX':0,
+	'shadowOffsetY':0,
+	'strokeStyle':'#000000',
+	'textAlign':'start',
+	'textBaseline':'alphabetic'
+}
+*/
--- js/lapiz.js
+++ js/lapiz.js
@@ -24,17 +24,47 @@
 				}
 				return destination;
 			};
-
 			for(i = 0;i < arguments.length;i++) {
 				dest = ext(dest,arguments[i]);
-	    }
-	    return dest;
+		    }
+		    return dest;
 		},		
 		/*
 		 * Private variables
 		*/
 		canvasList = [],
+		canvasListLength = 0,
 		idCounter = 0,
+		timeRender = null,
+
+		/*
+		 * Private methods
+		*/
+		findCanvasById = function(id){
+			var canvasToReturn = false;
+			for(var i=0;i<canvasListLength;i++){
+				if(canvasList[i].id == id){
+					canvasToReturn = canvasList[i];
+				}
+			}
+			return canvasToReturn;
+		},
+		canvasNodeId = function(node){
+			var idCanvas = node.getAttribute('id');
+			if(idCanvas == '' || idCanvas == undefined){
+				idCanvas = 'canvas-'+idCounter;
+				idCounter++;
+				node.setAttribute('id',idCanvas);				
+			}
+			return idCanvas;
+		},
+		setContextStyle = function(o){
+			for(var a in o){
+				if(typeof c[a] != 'undefined'){
+					c[a] = o[a];
+				}
+			}
+		},
 
 		/*
 		 * Shapes
@@ -50,14 +80,32 @@
 				var o = extend(shapeOptionsDefault,{
 					radius : 10				
 				},options);
-			}
+				setContextStyle(o);
+			},
+			'rectangle': function(options){
+				var o = extend(shapeOptionsDefault,{
+					width : 100,
+					height : 100
+				},options);
+				setContextStyle(o);
+
+				c.beginPath();
+				c.moveTo(o.x, o.y);
+				c.lineTo(o.x+o.width,o.y);
+				c.lineTo(o.x+o.width,o.y+o.height);
+				c.lineTo(o.x,o.y+o.height);
+				c.lineTo(o.x, o.y);
+				c.fill();
+				c.stroke();
+				c.closePath();
+			},
 		},
 
 		/*
 		 * Canvas Class
 		*/
-		cnv = function(canvas){
-			return this.init(canvas);
+		cnv = function(selection){
+			return this.init(selection);
 		},
 
 		/*
@@ -71,16 +119,60 @@
 	 * Canvas Definition
 	*/
 	cnv.prototype = {
-		init : function(shapeFunction, options){					
-			return this.reset().shape(shapeFunction, options);
+		init : function(selection){
+			this.node = document.getElementById(selection);
+			this.id = selection;
+			if(this.node != undefined){
+				this.context = this.node.getContext('2d');		
+			}else{
+				k('There is not canvas with id: '+selection+'.');
+			}
+
+			this.childs = [];
+			this.length = 0;
+
+			return this;			
 		},
+		append : function(sprite){
+			if(sprite.parent != null){
+				sprite.parent.detach(sprite);
+			}
+			this.childs.push(sprite);				
+			this.length++;
+			sprite.parent = this;
+			return this;
+		},
+		detach : function(sprite){
+			if(typeof sprite.spriteType != 'undefined'){
+				for(var i = 0; i < this.length; i++){
+					if(this.childs[i].id == sprite.id){
+						sprite.parent = null;
+						this.childs.splice(i,1);
+						this.length--;
+					}
+				}
+			}
+			return this;
+		},
+		render : function(){
+			c = this.context;
+			c.setTransform(1,0,0,1,0,0);
+			c.clearRect(0,0,this.node.width,this.node.height);
+			for(var i = 0; i < this.length; i++){
+				c.setTransform(1,0,0,1,0,0);
+				this.childs[i].render();
+			}
+		}
 	};
 
 	/*
 	 * Sprite Definition
 	*/
 	spr.prototype = {
-		init : function(shapeFunction, options){					
+		spriteType : true,
+		init : function(shapeFunction, options){
+			this.id = 'spr-'+ idCounter;
+			idCounter++;
 			return this.reset().shape(shapeFunction, options);
 		},
 		reset : function(){			
@@ -90,7 +182,6 @@
 			this.xScale = 1;
 			this.yScale = 1;
 			this.rotation = 0;
-
 			this.parent = null;
 			this.childs = [];
 			this.length = 0;			
@@ -109,27 +200,102 @@
 			this.shp = function(){shapeFunction(options);};
 			return this;
 		},
-		append : function(){
+		append : function(sprite){
+			if(typeof sprite.spriteType != 'undefined'){
+				if(sprite.parent != null){
+					sprite.parent.detach(sprite);
+				}
+				this.childs.push(sprite);				
+				this.length++;
+				sprite.parent = this;
+			}			
 			return this;
 		},
-		appendTo : function(){
+		appendTo : function(spriteOrCanvas){
+			if(typeof spriteOrCanvas.spriteType != 'undefined'){
+				//Sprite
+				spriteOrCanvas.append(this);
+			}else{
+				//Canvas
+				lapiz.getCanvas(spriteOrCanvas).append(this);
+			}
+			return this;
+		},
+		detach : function(sprite){
+			if(typeof sprite.spriteType != 'undefined'){
+				for(var i = 0; i < this.length; i++){
+					if(this.childs[i].id == sprite.id){
+						sprite.parent = null;
+						this.childs.splice(i,1);
+						this.length--;
+					}
+				}
+			}
 			return this;
 		},
 		transform : function(coordinates){
 
+		},
+		render : function(){
+			var rotRadians = this.rotation * Math.PI/180;
+			// Set Coordinates
+			c.translate(this.x,this.y);			
+			c.rotate(rotRadians);
+			c.scale(this.xScale,this.yScale);
+
+			// Render
+			this.shp();
+
+			// Render Childs
+			for(var i = 0; i < this.length; i++){
+				this.childs[i].render();
+			}
+
+			// Restore Coordinates
+			c.scale(1/this.xScale,1/this.yScale);
+			c.rotate(-rotRadians);			
+			c.translate(-this.x,-this.y);
+
+			return this;
 		}
 	};
 
 	/*
 	 * Lapiz
 	*/
-	var lapiz = {		
+	var lapiz = {
+		init : function(){
+			if(timeRender == null){
+				timeRender = setInterval(function(){
+					for(var i = 0; i < canvasListLength; i++){
+						canvasList[i].render();
+					}
+				},20);
+			}
+			return this;
+		},	
 		sprite : function(shapeFunction,options){
 			return new spr(shapeFunction,options);
+		},
+		getCanvas : function(selection){
+			var idSelection = (typeof selection == 'string') ? selection : canvasNodeId(selection),
+				preCanvas = findCanvasById(idSelection);
+
+			if(preCanvas){
+				return preCanvas;
+			}else{
+				var newCanvas = new cnv(idSelection);
+				canvasList.push(newCanvas);
+				canvasListLength++;
+				return newCanvas;
+			}
+		},
+		mostrar : function(){
+			
 		}
 	};
 
-	window.lapiz = lapiz;
+	window.lapiz = lapiz.init();
 })();
 
 kt = function(str){
